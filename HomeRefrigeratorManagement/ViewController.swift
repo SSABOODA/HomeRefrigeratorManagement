be1ce5c2b7d61a1888f4b4b96bb344a1bf4f1d92
SSABOODA <ssaboo92@gmail.com>
2023-09-26 17:30:41 +0900

    Initial Commit

---
+++ HomeRefrigeratorManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeRefrigeratorManagement
+//
+//  Created by 한성봉 on 2023/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
