817e2d23e8e912feca11b8dbfb083947991f4b16
SSABOODA <ssaboo92@gmail.com>
2023-09-26 18:44:05 +0900

    다크모드 초기 세팅, utility 폴더 구성

---
+++ HomeRefrigeratorManagement/Utility/Constant/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  HomeRefrigeratorManagement
+//
+//  Created by 한성봉 on 2023/09/26.
+//
+
+import Foundation
+
+extension Constant {
+    enum BaseColor {}
+    enum TitleColor {}
+}
---
+++ HomeRefrigeratorManagement/Utility/Constant/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  HomeRefrigeratorManagement
+//
+//  Created by 한성봉 on 2023/09/26.
+//
+
+import Foundation
+
+enum Constant {}
