4f3bf0610bb16fa76a2a3c59fd0fffc496cbdc86
SSABOODA <ssaboo92@gmail.com>
2023-10-07 17:08:49 +0900

    Food 아이콘 열거형 구조체 배열 완성

--- HomeRefrigeratorManagement/Utility/Constant/FoodConstant.swift
+++ HomeRefrigeratorManagement/Utility/Constant/FoodConstant.swift
@@ -28,6 +28,13 @@ extension Constant {
         // 김치/젓갈
     }
     
+    enum FoodStorageType: String, CaseIterable {
+        case all = "전체"
+        case outdoor = "실외"
+        case cold = "냉장"
+        case frozen = "냉동"
+    }
+    
     enum FoodConstant {
         static var foodIconInfo = [
             // "채소"
@@ -53,6 +60,13 @@ extension Constant {
             FoodModel(name: "양배추", category: .vegetable),
             FoodModel(name: "양상추", category: .vegetable),
             FoodModel(name: "아스파라거스", category: .vegetable),
+            FoodModel(name: "청경채", category: .vegetable),
+            FoodModel(name: "콩나물", category: .vegetable),
+            FoodModel(name: "파프리카", category: .vegetable),
+            FoodModel(name: "파", category: .vegetable),
+            FoodModel(name: "피망", category: .vegetable),
+            FoodModel(name: "호박", category: .vegetable),
+            FoodModel(name: "옥수수", category: .vegetable),
             // "과일"
             FoodModel(name: "감", category: .fruit),
             FoodModel(name: "귤", category: .fruit),
@@ -67,6 +81,12 @@ extension Constant {
             FoodModel(name: "수박", category: .fruit),
             FoodModel(name: "아보카도", category: .fruit),
             FoodModel(name: "오렌지", category: .fruit),
+            FoodModel(name: "체리", category: .fruit),
+            FoodModel(name: "크렌베리", category: .fruit),
+            FoodModel(name: "키위", category: .fruit),
+            FoodModel(name: "토마토", category: .fruit),
+            FoodModel(name: "파인애플", category: .fruit),
+            FoodModel(name: "포도", category: .fruit),
             // "곡식"
             FoodModel(name: "귀리", category: .grains),
             FoodModel(name: "쌀", category: .grains),
@@ -74,6 +94,9 @@ extension Constant {
             FoodModel(name: "땅콩", category: .nuts),
             FoodModel(name: "아몬드", category: .nuts),
             FoodModel(name: "잣", category: .nuts),
+            FoodModel(name: "캐슈넛", category: .nuts),
+            FoodModel(name: "피스타치오", category: .nuts),
+            FoodModel(name: "호두", category: .nuts),
             // "육류"
             FoodModel(name: "계란", category: .meat),
             FoodModel(name: "닭고기", category: .meat),
@@ -90,7 +113,11 @@ extension Constant {
             FoodModel(name: "생선", category: .seafood),
             FoodModel(name: "오징어", category: .seafood),
             FoodModel(name: "전복", category: .seafood),
+            FoodModel(name: "쭈꾸미", category: .seafood),
+            FoodModel(name: "조개", category: .seafood),
+            FoodModel(name: "해산물-직접입력", category: .seafood),
             // "양념"
+            FoodModel(name: "조미료-직접입력", category: .seasoning),
             // "조미료"
             FoodModel(name: "소금", category: .condiment),
             FoodModel(name: "설탕", category: .condiment),
@@ -100,6 +127,7 @@ extension Constant {
             FoodModel(name: "된장", category: .sauce),
             FoodModel(name: "마요네즈", category: .sauce),
             FoodModel(name: "소스-직접입력", category: .sauce),
+            FoodModel(name: "케첩", category: .sauce),
             // "면류"
             FoodModel(name: "면-직접입력", category: .noodles),
             // "유제품"
@@ -108,8 +136,11 @@ extension Constant {
             FoodModel(name: "요거트", category: .dairyProduct),
             FoodModel(name: "우유", category: .dairyProduct),
             FoodModel(name: "유제품-직접입력", category: .dairyProduct),
+            FoodModel(name: "치즈", category: .dairyProduct),
             // "음료"
+            FoodModel(name: "주류-직접입력", category: .beverage),
             // "인스턴스"
+            FoodModel(name: "참치캔", category: .instance),
             // "과자/제과"
             FoodModel(name: "과자", category: .snack),
             FoodModel(name: "베이글", category: .snack),
@@ -117,9 +148,7 @@ extension Constant {
             FoodModel(name: "시리얼", category: .snack),
             FoodModel(name: "식빵", category: .snack),
             // "기타"
-            
 
-            
         ]
         
         static let foodIconImageNameCollection = [
@@ -131,7 +160,6 @@ extension Constant {
             "생크림", "석류", "설탕", "소고기", "소금", "소스-직접입력", "수박", "시금치", "시리얼",
             "식빵", "쌀", "아몬드", "아보카도", "아스파라거스", "양고기", "양배추", "양상추",
             "연유", "오렌지", "오리고기", "오징어", "요거트", "우유", "유제품-직접입력", "잣", "전복",
-            //
             "조개", "조미료-직접입력", "주류-직접입력", "쭈꾸미", "참치캔", "청경채", "체리", "치즈",
             "캐슈넛", "케첩", "콩나물", "크렌베리", "키위", "토마토", "파", "파인애플", "파프리카",
             "포도", "피망", "피스타치오", "해산물-직접입력", "호두", "호박","옥수수"
