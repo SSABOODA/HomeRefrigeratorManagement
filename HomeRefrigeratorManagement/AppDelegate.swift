2b1885335f97ba0ca0d927f4cff312bf02ccc947
SSABOODA <ssaboo92@gmail.com>
2023-10-10 13:41:01 +0900

    IQKeyboardManagerSwift 추가

--- HomeRefrigeratorManagement/AppDelegate.swift
+++ HomeRefrigeratorManagement/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import FirebaseCore
 import RealmSwift
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         RealmTableRepository.shared.findFileURL()
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
