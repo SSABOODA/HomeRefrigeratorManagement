b7afcbbbdba2fcdd54c8fd6115247415f5ff21d6
SSABOODA <ssaboo92@gmail.com>
2023-10-17 15:12:58 +0900

    launchscreen

---
+++ HomeRefrigeratorManagement/View/Controllers/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  HomeRefrigeratorManagement
+//
+//  Created by 한성봉 on 2023/10/17.
+//
+
+import UIKit
+
+final class SplashViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
